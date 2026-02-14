176f2d17dddca36152b3fa9beeceeba2a84c5344
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2026-02-14 01:51:34 -0300

    update stow

--- stow.sh
+++ stow.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "🔹 Verificando mkinitcpio.conf antes do stow"
 
-# Se existir mkinitcpio.conf no sistema, mover para backup
+# Backup mkinitcpio.conf se existir
 if [ -f "/etc/mkinitcpio.conf" ]; then
     echo "🔹 Fazendo backup do mkinitcpio.conf atual"
     sudo mv /etc/mkinitcpio.conf /etc/mkinitcpio.conf.backup.$(date +%Y%m%d%H%M%S)
@@ -15,12 +15,26 @@ stow --verbose home -t ~
 echo "🔹 Aplicando dotfiles ETC"
 sudo stow --verbose etc -t /
 
+# =========================
+# BACKUP DOS ARQUIVOS BOOT
+# =========================
+
+if [ -f "/boot/EFI/refind/refind.conf" ]; then
+    echo "🔹 Fazendo backup do refind.conf"
+    sudo mv /boot/EFI/refind/refind.conf /boot/EFI/refind/refind.conf.backup.$(date +%Y%m%d%H%M%S)
+fi
+
+if [ -f "/boot/refind_linux.conf" ]; then
+    echo "🔹 Fazendo backup do refind_linux.conf"
+    sudo mv /boot/refind_linux.conf /boot/refind_linux.conf.backup.$(date +%Y%m%d%H%M%S)
+fi
+
 if [ -d "boot" ]; then
     echo "🔹 Aplicando dotfiles BOOT"
     sudo stow --verbose boot -t /
 fi
 
-# Só regenere initramfs se mkinitcpio.conf existir no stow
+# Regen initramfs se mkinitcpio.conf estiver nos dotfiles
 if [ -f "etc/etc/mkinitcpio.conf" ]; then
     echo "🔹 Regenerando initramfs"
     sudo mkinitcpio -P
