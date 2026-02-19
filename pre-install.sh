b5707b62dcb23e651e03edbccf7facfabf4e26e7
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2026-02-19 15:13:14 -0300

    git

--- pre-install.sh
+++ pre-install.sh
@@ -33,7 +33,7 @@ fi
 echo "✅ Aplicando dotfiles com Stow (Modular)..."
 cd "$DOTFILES_DIR"
 
-PACKAGES=(hypr rofi waybar keyboard themes)
+PACKAGES=(hypr rofi waybar keyboard themes git)
 
 for pkg in "${PACKAGES[@]}"; do
     if [ -d "$pkg" ]; then
